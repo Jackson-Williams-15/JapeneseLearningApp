98d8c750e5cda7565ce2ee9bb83527592f8444b6
Jackson Williams <wjack1201@yahoo.com>
2025-03-23 22:22:39 -0400

    validations for accounts

---
+++ JapaneseLearning.API/JapaneseLearning.API/Validation/EmailAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace JapaneseLearning.Validation;
+
+using System.ComponentModel.DataAnnotations;
+
+public class EmailAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(
+        object? value, ValidationContext validationContext)
+    {
+        if (value is string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return ValidationResult.Success;
+            }
+            catch
+            {
+                return new ValidationResult("Invalid email address");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
