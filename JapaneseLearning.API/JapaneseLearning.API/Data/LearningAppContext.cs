a25e9489f9d42d5da6c640026666224213156014
Jackson Williams <wjack1201@yahoo.com>
2025-04-07 16:29:40 -0400

    add user table to DB

--- JapaneseLearning.API/JapaneseLearning.API/Data/LearningAppContext.cs
+++ JapaneseLearning.API/JapaneseLearning.API/Data/LearningAppContext.cs
@@ -1,6 +1,6 @@
 using JapaneseLearning.Models;
 using Microsoft.EntityFrameworkCore;
-
+namespace JapaneseLearning.Data;
 public class LearningAppContext : DbContext
 {
     public LearningAppContext(DbContextOptions<LearningAppContext> options) : base(options) { }
@@ -8,9 +8,20 @@ public class LearningAppContext : DbContext
     public DbSet<VocabWords> Vocabulary { get; set; } = null!;
     public DbSet<ExampleSentences> ExampleSentences { get; set; }= null!;
     public DbSet<PartOfSpeech> PartsOfSpeech { get; set; }= null!;
+    public DbSet<Users> Users { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+
+        // Make sure the username is unique
+        modelBuilder.Entity<Users>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+
+        // Make sure the email is unique
+        modelBuilder.Entity<Users>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
 
         modelBuilder.Entity<PartOfSpeech>()
             .HasMany(ps => ps.ExampleSentences)
